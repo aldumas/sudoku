81b9c64939dce72596bc21ea80d76aaf76d287d6
Adam Dumas <thedumai+adam@gmail.com>
2022-03-17 15:30:58 -0700

    Replaced flat array with 2d array to simplify indexing.

--- GameBoard.php
+++ GameBoard.php
@@ -4,20 +4,47 @@ class GameBoard {
     private const BOARD_DIM = 9;
     private const SQUARE_DIM = 3;
     private const NUM_SQUARES_PER_DIM = self::BOARD_DIM / self::SQUARE_DIM;
+    private const ALL_NUMBERS = [1,2,3,4,5,6,7,8,9];
 
     private $board;
 
     public function __construct()
     {
-        $board = func_get_args(); // TODO make this a 2D array to simplify the math. 
+        $board = func_get_args();
 
+        if (empty($board)) {
+            throw new \InvalidArgumentException('missing argument(s)');
+        }
+
+        $this->board = is_array($board[0]) ?
+            self::create_board_from_2d_array($board[0]) :
+            self::create_board_from_1d_array($board);
+
+        // TODO validate types and values (1-9)
+    }
+
+    private static function create_board_from_2d_array($board)
+    {
+        if (count($board) !== self::BOARD_DIM) {
+            throw new \InvalidArgumentException('invalid number of rows');
+        }
+
+        foreach ($board as $i => $row) {
+            if (count($row) !== self::BOARD_DIM) {
+                throw new \InvalidArgumentException("invalid number of columns in row $i");
+            }
+        }
+
+        return $board;
+    }
+
+    private static function create_board_from_1d_array($board)
+    {
         if (count($board) != self::BOARD_DIM * self::BOARD_DIM) {
             throw new \InvalidArgumentException('invalid board size');
         }
 
-        // TODO check types of incoming values to ensure int and in range
-
-        $this->board = $board;
+        return array_chunk($board, self::BOARD_DIM);
     }
 
 
@@ -30,115 +57,96 @@ class GameBoard {
      */
     public function nextMoves() : array
     {
-
         //TODO distinguish no more moves but game unsolved and no more moves because game is solved?
 
-
-        $next_empty_index = $this->find_next_empty_space();
-        if (is_null($next_empty_index)) {
+        $empty_cell_coords = $this->find_next_empty_cell_coordinates();
+        if (is_null($empty_cell_coords)) {
             return []; // TODO caller needs to check if the board is solved.
         }
 
-        $legal_numbers = $this->calculate_legal_moves($next_empty_index);
+        $legal_numbers = $this->calculate_legal_moves($empty_cell_coords);
 
-        return array_map(function($number) use ($next_empty_index) { 
+        return array_map(function($number) use ($empty_cell_coords) { 
             $board = $this->board;
-            $board[$next_empty_index] = $number;
-            return new GameBoard(...$board);
+            $board[$empty_cell_coords->row][$empty_cell_coords->col] = $number;
+            return new self($board);
         }, $legal_numbers);
     }
 
-    private function find_next_empty_space()
+    private function find_next_empty_cell_coordinates()
     {
-        $next = array_search(0, $this->board);
-        return $next === false ? null : $next;
+        for ($row = 0; $row < self::BOARD_DIM; ++$row) {
+            $col = array_search(0, $this->board[$row]);
+            if ($col !== false)
+                return new Cell($row, $col);
+        }
+
+        return null;
     }
 
-    private function calculate_legal_moves($empty_index)
+    private function calculate_legal_moves($empty_cell_coords)
     {
-        static $digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
         // list of non-zero values in the current row
-        $current_row = $this->current_row($empty_index);
-        $legal_row_numbers = array_diff($digits, $current_row);
+        $current_row_numbers = $this->current_row_numbers($empty_cell_coords);
+        $legal_row_numbers = array_diff(self::ALL_NUMBERS, $current_row_numbers);
 
         // list of non-zero values in the current column
-        $current_col = $this->current_col_numbers($empty_index);
-        $legal_col_numbers = array_diff($digits, $current_col);
+        $current_col = $this->current_col_numbers($empty_cell_coords);
+        $legal_col_numbers = array_diff(self::ALL_NUMBERS, $current_col);
 
         // list of non-zero values in the current square
-        $current_square = $this->current_square($empty_index);
-        $legal_square_numbers = array_diff($digits, $current_square);
-
-        // TODO come back and think about whether preservation of keys is important here.
+        $current_square_numbers = $this->current_square_numbers($empty_cell_coords);
+        $legal_square_numbers = array_diff(self::ALL_NUMBERS, $current_square_numbers);
 
         return array_values(array_intersect($legal_row_numbers,
                                             $legal_col_numbers, 
                                             $legal_square_numbers));
     }
 
-    private function current_row($index)
+    private function current_row_numbers($cell_coords)
     {
-        return array_slice($this->board, self::row_num($index) * self::BOARD_DIM, self::BOARD_DIM);
+        return $this->board[$cell_coords->row];
     }
 
-    private static function row_num($index)
+    private function current_col_numbers($cell_coords)
     {
-        return floor($index / self::BOARD_DIM);
+        return array_map(function($row) use ($cell_coords) {
+            return $row[$cell_coords->col]; }, $this->board);
     }
 
-    private function current_col_numbers($index)
-    {
-        $col = self::col_num($index);
-
-        $num_squares_on_board = pow(self::SQUARE_DIM
-, 4);
-
-        $column_numbers = [];
-        for ($i = $col; $i < $num_squares_on_board; $i += self::BOARD_DIM) {
-            $column_numbers[] = $this->board[$i];
-        }
-
-        return $column_numbers;
-    }
-
-    private static function col_num($index)
-    {
-        return $index % self::BOARD_DIM;
-    }
-
-    private function current_square($index)
+    private function current_square_numbers($cell_coords)
     {
         $square_numbers = [];
 
-        $sqr_i = self::first_index_in_square($index);
-        for ($row = 0; $row < self::SQUARE_DIM
-; ++$row) {
-            for ($col = 0; $col < self::SQUARE_DIM
-    ; ++$col) {
-                $square_numbers[] = $this->board[$sqr_i++];
-            }
-            // set $sqr_i to first index on the next line inside the square
-            $sqr_i = $sqr_i - self::SQUARE_DIM
-     + self::BOARD_DIM;
+        $first_cell = self::first_cell_in_square($cell_coords);
+        for ($i = 0; $i < self::SQUARE_DIM; ++$i) {
+            $square_numbers = array_merge($square_numbers,
+                array_slice($this->board[$first_cell->row + $i], $first_cell->col, self::SQUARE_DIM));
         }
 
         return $square_numbers;
     }
 
-    private static function first_index_in_square($index)
+    private static function first_cell_in_square($cell_coords)
     {
-        // square number (based on row) * square length * row length + square number (based on col) * square length
-        $sqr_num_r = floor(self::row_num($index) / self::SQUARE_DIM);
-        $sqr_num_c = floor(self::col_num($index) / self::SQUARE_DIM);
+        $sqr_r = floor($cell_coords->row / self::SQUARE_DIM) * self::SQUARE_DIM;
+        $sqr_c = floor($cell_coords->col / self::SQUARE_DIM) * self::SQUARE_DIM;
 
-        return $sqr_num_r * self::SQUARE_DIM
- * self::BOARD_DIM + $sqr_num_c * self::SQUARE_DIM
-;
+        return new Cell($sqr_r, $sqr_c);
     }
 
     public function is_solved()
     {
-        return is_null($this->find_next_empty_space());
+        return is_null($this->find_next_empty_cell_coordinates());
+    }
+}
+
+class Cell {
+    public $row;
+    public $col;
+
+    public function __construct($row, $col) {
+        $this->row = $row;
+        $this->col = $col;
     }
 }
